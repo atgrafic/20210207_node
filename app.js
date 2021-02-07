2aae5fd75f4a60c8c5e7fa980b287acdaa05eff3
atgrafic <troczynska.anna@gmail.com>
2021-02-07 10:18:27 +0100

    add index i css

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-const functions = require("./functions");
+const express = require("express");
+const path = require('path');
 
-console.log("hello");
+const port = 3000;
+const app = express();
 
-functions.helloWord();
-functions.add(3, 2);
+
+app.set("view engine", "hbs");
+app.use('/assets', express.static(path.join(__dirname, "./assets")));t
+app.get('/',  function(req, res) {
+    res.render("index");
+    })
+    app.get('/about', function (req, res){
+        res.send("strona o nas")
+        })
+
+    app.listen(port, (err)=>{
+        if (err) {return console.log(err)}
+        console.log ("działa na porcie", port)
+    })
